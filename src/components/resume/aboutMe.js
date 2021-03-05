53dfd80581c63eb310c8d0fec42241b674721c5e
Hasip Timurtaş <hasip.timurtas@gmail.com>
2021-03-05 15:02:00 +0000

    CV updated, image and about me section changed.

--- src/components/resume/aboutMe.js
+++ src/components/resume/aboutMe.js
@@ -1,22 +1,22 @@
 import React from "react";
 
 const aboutMe = (props) => {
-  return (
-    <div class="row about-me">
-      <div class="col-md-2"></div>
-      <div class="col-md-10">
-        <p class="text-indent">
-          Hi! I'm Hasip, a {new Date().getFullYear() - 1991} years old developer based in Malta, I specialise in building web apps. My career started
-          at an early age, when I was 16 I started programming. My first "good" program was a damage calculation software in high school. With the
-          pass of the years, I decided to transform my hobby into a profession so I studied programming in Karabuk University. I consider myself a
-          full-stack developer. I'm currently working in a company in Malta, I enjoy developing web applications and learning new technologies for
-          developing faster and better. I am a self-taught person, I love learning new things and improving my skills in different web platforms such
-          as Udemy, teamtreehouse and youtube. I like Javascript (Javascript is everywhere : ) , coffee, linux and sometimes playing video games, I
-          enjoy building stuff, investigating new technologies.
-        </p>
-      </div>
-    </div>
-  );
+    return (
+        <div class="row about-me">
+            <div class="col-md-2"></div>
+            <div class="col-md-10">
+                <p class="text-indent">
+                    Hi! I'm Hasip, a {new Date().getFullYear() - 1991} years old developer based in Malta,  I specialize in building web apps.
+                    My career started at an early age, when I was 16 I started programming.
+                    My first "good" program was damage calculation software in high school. With the pass of the years,
+                    I decided to transform my hobby into a profession so I studied programming at Karabuk University.
+                    I consider myself a full-stack developer. I enjoy developing web applications and learning new technologies for developing faster and better.
+                    I am a self-taught person, I love learning new things and improving my skills in different web platforms such as Udemy, teamtreehouse and youtube.
+                    I like Javascript, coffee, Linux and playing chess. I enjoy building stuff, investigating new technologies.
+                </p>
+            </div>
+        </div>
+    );
 };
 
 export default aboutMe;
--- src/components/resume/skills.js
+++ src/components/resume/skills.js
@@ -1,24 +1,26 @@
 import React from "react";
 
 const header = (props) => {
-  return (
-    <div className="row">
-      <div className="col-md-2"></div>
-      <div className="col-md-10">
-        <li>Advanced Javascript knowledge</li>
-        <li>Good knowledge with NodeJS, ReactJS and VueJS</li>
-        <li>Good knowledge in HTML5, CSS3</li>
-        <li>Experience with various CSS frameworks, Bootstrap, Material Design and TailwindCss </li>
-        <li>Experience debugging for compatibility issues.</li>
-        <li>Proficiency in integrating with REST APIs</li>
-        <li>Experience with MVC, OOP, design-patterns</li>
-        <li>Experience with server-side languages and architectures PHP, C#</li>
-        <li>Experience with Git and Mercurial Version Controls </li>
-        <li>Experience with Linux terminal</li>
-        <li>Knowledge and practical use of cloud-based hosting, AWS, DigitalOccean, Heroku, firebase</li>
-      </div>
-    </div>
-  );
+    return (
+        <div className="row">
+            <div className="col-md-2"></div>
+            <div className="col-md-10">
+                <li>Advanced Javascript knowledge, with and without libraries</li>
+                <li>Good knowledge of NodeJS, ReactJS and Typescript</li>
+                <li>Experience with MeteorJS, VueJS</li>
+                <li>Good knowledge of HTML5, CSS3</li>
+                <li>Experience with various CSS frameworks such as Bootstrap, Material Design, TailWindCSS </li>
+                <li>Experience debugging for compatibility issues</li>
+                <li>Proficiency in integrating with REST APIs</li>
+                <li>Experience with MVC, OOP, design-patterns</li>
+                <li>Experience with server-side languages and architectures</li>
+                <li>Experience with Git, Gihub and Mercurial</li>
+                <li>Experience with Linux, Docker, Microservices</li>
+                <li>Knowledge and practical use of cloud computing, AWS, Google cloud, firebase</li>
+                <li>Old experience with C#, python and PHP</li>
+            </div>
+        </div>
+    );
 };
 
 export default header;
